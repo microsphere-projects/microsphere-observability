719aabe59855d8e03f3cb524ea138e6782898f4f
Mercy Ma <mercyblitz@gmail.com>
2026-09-02 20:49:01 +0800

    Add Spring Cloud metrics auto-config
    Adds a new Spring Cloud metrics module that customizes Micrometer registries with an `instance` tag from the current service registration. Registers the auto-configuration for Spring Boot 2/3 discovery and adds tests covering the auto-config and tag wiring.

---
+++ microsphere-observability-metrics-spring-cloud/src/main/java/io/microsphere/metrics/micrometer/spring/cloud/actuate/autoconfigure/ServiceRegistrationMetricsAutoConfiguration.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.microsphere.metrics.micrometer.spring.cloud.actuate.autoconfigure;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.microsphere.metrics.micrometer.spring.boot.actuate.condition.ConditionalOnMicrometerAvailable;
+import io.microsphere.spring.cloud.client.service.registry.condition.ConditionalOnAutoServiceRegistrationAvailable;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.cloud.client.ConditionalOnDiscoveryEnabled;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+;
+
+/**
+ * Micrometer Auto-Configuration
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see MeterRegistry
+ * @see org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration
+ * @see org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration
+ * @since 1.0.0
+ */
+@ConditionalOnDiscoveryEnabled
+@ConditionalOnMicrometerAvailable
+@ConditionalOnAutoServiceRegistrationAvailable
+@AutoConfigureAfter(name = {
+        "org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration",
+        "org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration",
+        "io.microsphere.spring.cloud.client.service.registry.autoconfigure.SimpleAutoServiceRegistrationAutoConfiguration"
+})
+@Configuration(proxyBeanMethods = false)
+public class ServiceRegistrationMetricsAutoConfiguration {
+
+    public static final String INSTANCE_TAG_KEY = "instance";
+
+    @Bean
+    public MeterRegistryCustomizer commonMeterRegistryCustomizer(
+            ObjectProvider<Registration> registrationProvider) {
+        return registry -> {
+            configureCommonTags(registry, registrationProvider);
+        };
+    }
+
+    private void configureCommonTags(MeterRegistry registry, ObjectProvider<Registration> registrationProvider) {
+        registrationProvider.ifAvailable(registration -> {
+            String host = registration.getHost();
+            String instance = host + ":" + registration.getPort();
+            registry.config().commonTags(INSTANCE_TAG_KEY, instance);
+        });
+    }
+}
---
+++ microsphere-observability-metrics-spring-cloud/src/test/java/io/microsphere/metrics/micrometer/spring/cloud/actuate/autoconfigure/ServiceRegistrationMetricsAutoConfigurationIntegrationTest.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.microsphere.metrics.micrometer.spring.cloud.actuate.autoconfigure;
+
+import io.micrometer.core.instrument.Meter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static io.microsphere.metrics.micrometer.spring.cloud.actuate.autoconfigure.ServiceRegistrationMetricsAutoConfiguration.INSTANCE_TAG_KEY;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+/**
+ * {@link ServiceRegistrationMetricsAutoConfiguration} Integration Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @see ServiceRegistrationMetricsAutoConfiguration
+ * @since 1.0.0
+ */
+@SpringBootTest(
+        classes = {
+                ServiceRegistrationMetricsAutoConfigurationIntegrationTest.class
+        },
+        webEnvironment = RANDOM_PORT,
+        properties = {
+                "microsphere.spring.cloud.service-registry.auto-registration.simple.enabled=true"
+        }
+)
+@EnableAutoConfiguration
+public class ServiceRegistrationMetricsAutoConfigurationIntegrationTest {
+
+    @Autowired
+    private MeterRegistry registry;
+
+    @Test
+    void test() {
+        List<Meter> meters = registry.getMeters();
+        meters.stream().map(Meter::getId).forEach(meterId -> {
+            assertNotNull(meterId.getTag("application"));
+            assertNotNull(meterId.getTag(INSTANCE_TAG_KEY));
+            assertNotNull(meterId.getTag("profiles"));
+        });
+    }
+}
